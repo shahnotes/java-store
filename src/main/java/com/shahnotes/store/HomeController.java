56d2e91f304e1ad3bbab4a8397c7df33f3f0900b
Harsh Shah <shahharsh.210995@gmail.com>
2025-03-09 12:10:58 +0530

    Thymeleaf integration

--- src/main/java/com/shahnotes/store/HomeController.java
+++ src/main/java/com/shahnotes/store/HomeController.java
@@ -1,17 +1,12 @@
 package com.shahnotes.store;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-    @Value("${spring.application.name}")
-    private String appName;
-
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
-        return "index.html";
+        return "index";
     }
 }
